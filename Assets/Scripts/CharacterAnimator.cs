cf1a9906dc232d7d343f020216971c319fff6519
unknown <dsan2@calstatela.edu>
2020-10-03 19:25:02 -0700

    Added Justine's practice Scene

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    const float LocomotionSmoothTime = 0.1f;
+    Animator animator;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, LocomotionSmoothTime, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryItem : MonoBehaviour, IInventoryItem
+{
+    public Vector3 PickupPosition;
+    public Vector3 PickupRotation;
+
+    public string Name => throw new System.NotImplementedException();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void OnUse()
+    {
+    
+    }
+
+    public void OnPickup()
+    {
+        transform.localPosition = PickupPosition;
+        transform.localEulerAngles = PickupPosition;
+        gameObject.SetActive(true);
+        Debug.Log("the coords are ");
+        Debug.Log(PickupPosition);
+        Debug.Log(PickupRotation);
+    }
+}
