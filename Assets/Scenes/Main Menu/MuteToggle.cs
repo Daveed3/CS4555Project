71b2c80632618bcbd893501a7a254ce9f0dfc222
Daveeed3 <dsan2@calstatela.edu>
2020-11-12 13:52:38 -0800

    Added Peter's Menu Scene pt1

---
+++ Assets/Scenes/Main Menu/MuteToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class MuteToggle : MonoBehaviour
+{
+    Toggle muteToggle;
+
+    void Start()
+    {
+        muteToggle = GetComponent<Toggle>();
+        if(AudioListener.volume == 0)
+        {
+            muteToggle.isOn = false;
+        }
+    }
+
+    public void ToggleAudioOnValueChange(bool audioIn)
+    {
+        if(audioIn)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            
+            AudioListener.volume = 0;
+            
+        }
+    }
+    
+}
